3dca84b9a527b445f1079bdddc3d7deec69a8937
evan-benoit <evan.benoit@expel.io>
2024-04-12 07:00:31 -0700

    added some hardcoded vertexAI calls!

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import base64
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, FinishReason
+import vertexai.preview.generative_models as generative_models
 import os
 
 from flask import Flask
@@ -5,12 +9,53 @@ from flask import Flask
 app = Flask(__name__)
 
 
+
+def generate():
+
+    text1 = """Give me a 1000 word summary of the english premier league season from the point of view of tottenham hotspur.  
+            Note that the season is still in progress.  
+            Do not name any managers or players, but mention specific teams that they played.  
+            Emphasize derbies.  
+            Summarize the start of the season, middle of the season, and then give the latest progress as of April 2024, summarizing their last four games"""
+
+    generation_config = {
+        "max_output_tokens": 8192,
+        "temperature": 1,
+        "top_p": 0.95,
+    }
+
+    safety_settings = {
+        generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    }
+
+    vertexai.init(project="evcon-app", location="us-central1")
+    model = GenerativeModel("gemini-1.5-pro-preview-0409")
+    responses = model.generate_content(
+        [text1],
+        generation_config=generation_config,
+        safety_settings=safety_settings,
+        stream=True,
+    )
+
+    responseString = ""
+    for response in responses:
+        responseString += response.text
+
+    return responseString
+
+
+
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
     name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+    return generate()
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
