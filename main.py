e1e99f1b552c59ca3fb6f56daeb7444096481423
evan-benoit <evan.benoit@expel.io>
2024-04-19 06:39:39 -0400

    added CORS headers

--- main.py
+++ main.py
@@ -59,6 +59,20 @@ def generate(teamList, data):
 
 @app.route("/summary", methods = ['GET'])
 def summaryEndpoint():
+
+    if request.method == "OPTIONS":
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            # allow requests from trophypace.com and localhost
+            "Access-Control-Allow-Origin": "https://trophypace.com, http://localhost:1234",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
+
+        return ("", 204, headers)
+
     # get the team name, country code, season, and league from the request
     teamList = str(request.args.get('teamList'))
     countryCode = str(request.args.get('countryCode'))
